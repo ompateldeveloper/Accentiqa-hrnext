5473811d4b52b28c747db35a5cf90293ba453f6b
satish <nagakancherla123@gmail.com>
2024-02-28 18:25:10 +0530

    files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Navbar from "./components/Navbar"
+import Employeeposition from "./pages/Employeeposition"
+import Payment from "./pages/Payment"
+import Statutoryinfo from "./pages/Statutoryinfo"
 
 function App() {
     return (
         <div>
             <Navbar />
+            <Employeeposition />
+            <Statutoryinfo />
+            <Payment />
         </div>
     )
 }
