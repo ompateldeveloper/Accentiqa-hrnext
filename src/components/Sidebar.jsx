7d9014415a1d53f7443e22a568ba02ba4ee8970c
ompateldeveloper <ompatel.developer@gmail.com>
2024-03-04 10:31:25 +0530

    updated color to theme-1

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ export default function Sidebar() {
 
     return (
         <div className={cn('h-[calc(100vh-80px)] fixed flex mt-20 bg-white z-20 duration-100 lg:-translate-x-full')}>
-            <div className="fixed-panel w-16  bg-[#5872E3] text-white">
+            <div className="fixed-panel w-16  bg-theme-1 text-white">
                 <TabsList>
                     <TabsTrigger className='p-4 w-full' active='bg-white text-zinc-800 ' tab={tabs} tabIndex={'Home'} selectTab={selectTab} icon={<Home/>}></TabsTrigger>
                     <TabsTrigger className='p-4 w-full' active='bg-white text-zinc-800 ' tab={tabs} tabIndex={'Add Employee'} selectTab={selectTab} icon={<UserPlus/>}></TabsTrigger>
